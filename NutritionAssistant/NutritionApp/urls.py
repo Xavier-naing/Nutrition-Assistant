b3faca9dc1384d8becd2f4d56febf51c0f4e92f5
Yaungzawae <yezaw07aung@gmail.com>
2025-01-26 23:58:49 +0700

    Improved Search with offsets and decent UI

--- NutritionAssistant/NutritionApp/urls.py
+++ NutritionAssistant/NutritionApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("home", views.home, name="home"),
     path("todos", views.todosView, name="todos"),
-    path("result", views.search, name="result")
+    path("result/", views.search, name="result"),
+    path("result/<str:name>", views.search, name="result")
 ]
--- NutritionAssistant/NutritionApp/views.py
+++ NutritionAssistant/NutritionApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from .models import TodoItem
 from .forms import searchForm 
 
@@ -8,45 +8,45 @@ import json
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 conn = http.client.HTTPSConnection("spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
 a = os.getenv("API_KEY")
-print(a)
 
 headers = {
-    'x-rapidapi-key': os.environ.get('API_KEY'),
+    'x-rapidapi-key': os.getenv('API_KEY'),
     'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
     }
+
 
 # Create your views here.
 def home(request):
     form = searchForm(request.POST)
-
     return render(request, "home.html", {'form' : form})
 
-
 def todosView(request):
-
     return render(request, "todos.html", {"todos": TodoItem.objects.all()})
 
-def search(request):
-    search_url = ""
+def search(request, *args, **kwargs):
+    name, current = "", 0
     form = searchForm(request.POST)
 
-    name = ""
-    if form.is_valid():
-        if form.cleaned_data['name']:
-            name = form.cleaned_data['name']
-        # if form.cleaned_data['']
-        print(name)
+    if request.method == "POST":
+        if form.is_valid():
+            if form.cleaned_data['name']:
+                name = form.cleaned_data['name']
+    if request.method == "GET":
+        name = kwargs['name']
+        current = request.GET.get('current')
 
-    
-    conn.request("GET", f"/recipes/complexSearch?query={name}&instructionsRequired=true&fillIngredients=false&addRecipeInformation=false&addRecipeInstructions=false&addRecipeNutrition=false&sort=max-used-ingredients&offset=0&number=10", headers=headers)
+    current = int(current)
+    conn.request("GET", f"/recipes/complexSearch?query={name}&instructionsRequired=true&fillIngredients=false&addRecipeInformation=false&addRecipeInstructions=false&addRecipeNutrition=false&sort=max-used-ingredients&offset={current * 10}&number=10", headers=headers)
     
     res = conn.getresponse()
     data = res.read()
     data = json.loads(data.decode("utf-8"))
-    print(data)
+    maximum = data["totalResults"] // 10
+    totalResult = list(range( max(current-2, 0), min(current+6, maximum+1)))
 
-    return render(request, "result.html", {"result": data['results']})
+    return render(request, "result.html", {"search" : name ,"result": data['results'], "totalResult" : totalResult, "current" : current, "maximum": maximum })
